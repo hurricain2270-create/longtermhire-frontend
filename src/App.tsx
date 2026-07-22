f4d18cd390b41a5b4566fcfd1a9e0594deb19e14
hurricain2270-create <hurricain2270@gmail.com>
2026-07-23 00:55:00 +1000

    Fix App.tsx broken import with exact string match

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import 
-        <Route path="/hire-management" element={<PrivateRoute allowedRoles={["super_admin"]}><DashboardLayout><HireManagement /></DashboardLayout></PrivateRoute>} />{ BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AdminSidebar from "./AdminSidebar";
