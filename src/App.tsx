810999652e268925581f1d036345cfd12b627160
hurricain2270-create <hurricain2270@gmail.com>
2026-07-23 12:33:46 +1000

    Re-add hire-management route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import 
+        <Route path="/hire-management" element={<PrivateRoute allowedRoles={["super_admin"]}><DashboardLayout><HireManagement /></DashboardLayout></PrivateRoute>} />{ BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AdminSidebar from "./AdminSidebar";
